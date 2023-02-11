b7e1f5ab9bea66d1e033c5d5f4f43bdac11e65f2
Dakshatha0 <kasturidakshatha@gmail.com>
2023-02-11 11:22:37 +0530

    commit

--- lib/src/department/CSE/year/year1/year1sem1/Subjects/BEE.dart
+++ lib/src/department/CSE/year/year1/year1sem1/Subjects/BEE.dart
@@ -1,33 +1,47 @@
 import 'package:flutter/material.dart';
 
-class BEE extends StatefulWidget {
-  const BEE({Key? key}) : super(key: key);
-
-  @override
-  _BEE createState() => _BEE();
-}
-class _BEE extends State<BEE> {
+class BEE extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          RichText(
-            text:TextSpan(
-              text: 'Hello',
-              style: DefaultTextStyle.of(context).style,
-              children: const <TextSpan>[
-                TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
-                TextSpan(text: 'world!'),
-              ]
-            )
-          )
-        ],
-      )),
-
-    );
+      return Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Heading 1',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Heading 2',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width / 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
--- lib/src/department/CSE/year/year1/year1sem1/Subjects/M1.dart
+++ lib/src/department/CSE/year/year1/year1sem1/Subjects/M1.dart
@@ -10,126 +10,14 @@ class _Mathematics1 extends State<Mathematics1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.black,
       appBar: null,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children:[
-                // Expanded(
-                  Expanded(
-                      child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Text("UNIT - I: Matrices\n", textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.lightBlue,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.combine([
-                          TextDecoration.underline,
-                          TextDecoration.overline
-                        ]),
-                        decorationThickness: 0.5,
-                        letterSpacing: 1.0,
-                        wordSpacing: 4.0
-                      ),),
-                    )
-                ),
-                Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Text('''Matrices: Types of Matrices, Symmetric; Hermitian; Skew-symmetric; Skew-Hermitian;
-                                  orthogonal matrices; Unitary Matrices; rank of a matrix by Echelon form and Normal form,
-                                  Inverse of Non-singular matrices by Gauss-Jordan method; System of linear equations;
-                                  solving system of Homogeneous and Non-Homogeneous equations. Gauss elimination
-                                  method; Gauss Seidel Iteration Method.''',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                        ),),
-                    )
-                ),
-                Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Text("UNIT - II: Eigen Value and Vectors", textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.lightBlue,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.combine([
-                              TextDecoration.underline,
-                              TextDecoration.overline
-                            ]),
-                            decorationThickness: 0.5,
-                            letterSpacing: 1.0,
-                            wordSpacing: 4.0
-                        ),),
-                    )
-                ),
-                  Expanded(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: Text("UNIT - III: Sequence and Series", textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.lightBlue,
-                              fontWeight: FontWeight.bold,
-                              decoration: TextDecoration.combine([
-                                TextDecoration.underline,
-                                TextDecoration.overline
-                              ]),
-                              decorationThickness: 0.5,
-                              letterSpacing: 1.0,
-                              wordSpacing: 4.0
-                          ),),
-                      )
-                  ),
-                Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Text("UNIT - IV: Calculus", textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.lightBlue,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.combine([
-                              TextDecoration.underline,
-                              TextDecoration.overline
-                            ]),
-                            decorationThickness: 0.5,
-                            letterSpacing: 1.0,
-                            wordSpacing: 4.0
-                        ),),
-                    )
-                ),
-                Expanded(
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.vertical,
-                      child: Text("UNIT - V: Multivariable Calculus", textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.lightBlue,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.combine([
-                              TextDecoration.underline,
-                              TextDecoration.overline
-                            ]),
-                            decorationThickness: 0.5,
-                            letterSpacing: 1.0,
-                            wordSpacing: 4.0
-                        ),),
-                    )
-                ),
-                // ),
-              ],
-            )
-          ],
-        )
-      ),
+      body: Center(child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+
+        ],
+      )),
 
     );
   }
