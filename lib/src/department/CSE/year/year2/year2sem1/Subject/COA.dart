f074ba8227a4036b1b6461f152eac8307d851c5e
Atoshi <116013768+Atoshi-Das@users.noreply.github.com>
2023-02-16 15:20:49 +0530

    Update COA.dart

--- lib/src/department/CSE/year/year2/year2sem1/Subject/COA.dart
+++ lib/src/department/CSE/year/year2/year2sem1/Subject/COA.dart
@@ -10,15 +10,110 @@ class _COA extends State<COA> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-        ],
-      )),
-
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'UNIT-I:',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  " Digital Computers: Introduction, Block diagram of Digital Computer, Definition of Computer Organization, Computer Design and Computer Architecture.\n Register Transfer Language and Micro operations: Register Transfer language, Register Transfer, Bus and memory transfers, Arithmetic Micro operations, logic micro operations, shift micro operations,  Arithmetic logic shift unit.\n Basic Computer Organization and Design: Instruction codes, Computer Registers Computer instructions, Timing and Control, Instruction cycle, Memory Reference Instructions, Input – Output and Interrupt. ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-II:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Microprogrammed Control: Control memory, Address sequencing, micro program example, design of control unit \n Central Processing Unit: General Register Organization, Instruction Formats, Addressing modes, Data Transfer and Manipulation, Program Control.",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-III:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Data Representation: Data types, Complements, Fixed Point Representation, Floating Point Representation.\nComputer Arithmetic: Addition and subtraction, multiplication Algorithms, Division Algorithms, Floating – point Arithmetic operations. Decimal Arithmetic unit, Decimal Arithmetic operations.",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-IV: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Input-Output Organization: Input-Output Interface, Asynchronous data transfer, Modes of Transfer,Priority Interrupt Direct memory Access.\n Memory Organization: Memory Hierarchy, Main Memory, Auxiliary memory, Associate Memory, Cache Memory.",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'Unit-V: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Reduced Instruction Set Computer: CISC Characteristics, RISC Characteristics. \n Pipeline and Vector Processing: Parallel Processing, Pipelining, Arithmetic Pipeline, Instruction Pipeline, RISC Pipeline, Vector Processing, Array Processor.\n Multi Processors: Characteristics of Multiprocessors, Interconnection Structures, Interprocessor arbitration, Interprocessor communication and synchronization, Cache Coherence.",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
