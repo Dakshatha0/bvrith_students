7bfca3a28b266ce6ce6fff52058b21c78131da1a
UppalapatiSri <93474927+UppalapatiSri@users.noreply.github.com>
2023-02-16 14:15:20 +0530

    Update DBMS.dart
    DBMS completed

--- lib/src/department/CSE/year/year2/year2sem2/Subjects/DBMS.dart
+++ lib/src/department/CSE/year/year2/year2sem2/Subjects/DBMS.dart
@@ -10,15 +10,110 @@ class _DBMS extends State<DBMS> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-        ],
-      )),
-
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'UNIT-I:',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  " Database System Applications: A Historical Perspective, File Systems versus a DBMS, the Data Model, Levels of Abstraction in a DBMS, Data Independence, Structure of a DBMS Introduction to Database Design: Database Design and ER Diagrams, Entities, Attributes, and Entity Sets, Relationships and Relationship Sets, Additional Features of the ER Model, Conceptual Design With the ER Mode ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-II:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Introduction to the Relational Model: Integrity constraint over relations, enforcing integrity constraints, querying relational data, logical data base design, introduction to views, destroying/altering tables and views. Relational Algebra, Tuple relational Calculus, Domain relational calculus",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-III:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "SQL: QUERIES, CONSTRAINTS, TRIGGERS: form of basic SQL query, UNION, INTERSECT, and EXCEPT, Nested Queries, aggregation operators, NULL values, complex integrity constraints in SQL, triggers and active data bases. Schema Refinement: Problems caused by redundancy, decompositions, problems related to decomposition, reasoning about functional dependencies, FIRST, SECOND, THIRD normal forms, BCNF, lossless join decomposition, multi-valued dependencies, FOURTH normal form, FIFTH normal form. ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-IV: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Transaction Concept, Transaction State, Implementation of Atomicity and Durability, Concurrent Executions, Serializability, Recoverability, Implementation of Isolation, Testing for serializability, Lock Based Protocols, Timestamp Based Protocols, Validation- Based Protocols, Multiple Granularity, Recovery and Atomicity, Log–Based Recovery, Recovery with Concurrent Transactions.",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'Unit-V: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Data on External Storage, File Organization and Indexing, Cluster Indexes, Primary and Secondary Indexes, Index data Structures, Hash Based Indexing, Tree base Indexing, Comparison of File Organizations, Indexes and Performance Tuning, Intuitions for tree Indexes, Indexed Sequential Access Methods (ISAM), B+ Trees: A Dynamic Index Structure.",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
