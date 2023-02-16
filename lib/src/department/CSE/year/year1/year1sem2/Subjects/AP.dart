6acff8b7f7cb77a25d4f1ba251bebded3bb324fd
UppalapatiSri <20wh1a0574@bvrithyderabad.edu.in>
2023-02-16 15:30:20 +0530

    trying

--- lib/src/department/CSE/year/year1/year1sem2/Subjects/AP.dart
+++ lib/src/department/CSE/year/year1/year1sem2/Subjects/AP.dart
@@ -10,15 +10,110 @@ class _AppliedPhysics extends State<AppliedPhysics> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-        ],
-      )),
-
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'UNIT-I:',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "  ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-II:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-III:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-IV: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Text(
+                'Unit-V: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/src/department/IT/year/yearofstudy.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:bvrith_students/src/department/CSE/year/year1/year1sem.dart';
+import 'package:bvrith_students/src/department/CSE/year/year2/year2sem.dart';
+import 'package:bvrith_students/src/department/CSE/year/year3/year3sem.dart';
+import 'package:bvrith_students/src/department/CSE/year/year4/year4sem.dart';
+class yearOfstudy extends StatefulWidget {
+  const yearOfstudy({Key? key}) : super(key: key);
+
+  @override
+  State<yearOfstudy> createState() => _yearOfStudyState();
+}
+class _yearOfStudyState extends State<yearOfstudy> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: null,
+      body: Center(child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => yearOneSem()));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 8,horizontal: 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+            ),
+            child: Text("I-YEAR",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => yearTwoSem()));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 8,horizontal: 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+            ),
+            child: Text("II-YEAR",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => yearThirdSem()));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 8,horizontal: 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+            ),
+            child: Text("III-YEAR",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => yearFourSem()));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 8,horizontal: 55),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+            ),
+            child: Text("IV-YEAR",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+          ),
+        ],
+      )),
+
+    );
+  }
+}
+
+
+
+
+
+// body: Center(
+// child: ElevatedButton(
+// style: ElevatedButton.styleFrom(minimumSize: const Size(200, 50)),
+// child: Text(
+// "cse".toUpperCase(),
+// style: const TextStyle(fontWeight: FontWeight.bold),
+// ),
+// onPressed: (){
+// Navigator.push(
+// context,
+// MaterialPageRoute(builder: (context){
+// return yearOfstudy();
+// }
+// ));
+// },
+// ),
+//
+// )
