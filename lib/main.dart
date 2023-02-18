bd3292e456e87a58705308c796b1a1f1067bb28c
Dakshatha0 <kasturidakshatha@gmail.com>
2023-02-18 20:15:29 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:bvrith_students/src/%20College%20Resources/resourcesforcollege.dart';
 import 'package:bvrith_students/src/Links%20to%20Sites/sidemenu.dart';
 import 'package:bvrith_students/src/Placement%20Material/placementmaterial.dart';
 import 'package:flutter/material.dart';
 import 'src/department/department.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(
+      const MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "R18 REGULATION",
     home: MyApp(),
@@ -20,57 +22,58 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       drawer: sidemenu(),
       backgroundColor: Colors.black,
+      // floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
       floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
-      appBar: null,
+      appBar: AppBar(
+        title: Text('',style: TextStyle(backgroundColor: Colors.black ),),
+        backgroundColor: Colors.black,
+      ),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => sidemenu())
-                  );
-                },
-                child: const Icon(Icons.home),
+          children: [
+            ElevatedButton(
+              child: Text("R18 REGULATION SYLLABUS", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+              onPressed: (){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => department())
+                );
+              },
+              style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.white,
-                focusColor: Colors.white,
-                foregroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 8,horizontal: 110),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
               ),
-        ElevatedButton(
-          child: Text("R18 REGULATION SYLLABUS", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-          onPressed: (){
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => department())
-            );
-          },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.white,
-            padding: EdgeInsets.symmetric(vertical: 8,horizontal: 35),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-          ),
+            ),
+            ElevatedButton(
+              child: Text("PLACEMENT MATERIAL", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+              onPressed: (){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => placementmaterial())
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 8,horizontal: 117),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+              ),
+            ),
+            ElevatedButton(
+              child: Text("ACADEMIC RESOURCES", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+              onPressed: (){
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => resourcesforcollege())
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 8,horizontal: 95),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+              ),
+            ),
+          ],
         ),
-              ElevatedButton(
-                child: Text("PLACEMENT MATERIAL", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                onPressed: (){
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => placementmaterial())
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  padding: EdgeInsets.symmetric(vertical: 8,horizontal: 47),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
-                ),
-              ),
-        ],
-      ),
       ),
     );
   }
 }
-
-
-
-
-
--- lib/src/department/CSE/year/year3/year3sem2/Subjects/CD.dart
+++ lib/src/department/CSE/year/year3/year3sem2/Subjects/CD.dart
@@ -10,15 +10,120 @@ class _CD extends State<CD> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-        ],
-      )),
-
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'UNIT-I:',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-II:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-III:  ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  " ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-IV: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'Unit-V: ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
