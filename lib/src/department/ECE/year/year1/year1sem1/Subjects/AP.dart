cdbb73e58372df4574f1a51dfc3a72b062e0c1ae
UppalapatiSri <20wh1a0574@bvrithyderabad.edu.in>
2023-02-28 18:55:28 +0530

    ECE first year

--- lib/src/department/ECE/year/year1/year1sem1/Subjects/AP.dart
+++ lib/src/department/ECE/year/year1/year1sem1/Subjects/AP.dart
@@ -16,10 +16,11 @@ class _AppliedPhysics extends State<AppliedPhysics> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'UNIT-I:',
+                'UNIT-I: Quantum Mechanics',
                 style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontSize: MediaQuery.of(context).size.width / 36,
                   fontWeight: FontWeight.bold,
+                  color: Colors.orange,
                 ),
               ),
               SizedBox(
@@ -28,85 +29,94 @@ class _AppliedPhysics extends State<AppliedPhysics> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "  ",
+                  "Introduction to quantum physics, Black body radiation, Planck’s law, Photoelectric effect, Compton effect, de-Broglie’s hypothesis, Wave-particle duality, Davisson and Germer experiment, Heisenberg’s Uncertainty principle, Born’s interpretation of the wave function, Schrodinger’s time independent wave equation, Particle in one dimensional box.",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
               ),
               Text(
-                'UNIT-II:  ',
+                'UNIT-II: Semiconductor Physics ',
                 style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontSize: MediaQuery.of(context).size.width / 36,
                   fontWeight: FontWeight.bold,
+                  color: Colors.orange,
                 ),
               ),
               SizedBox(
-                height: 16,
+                height: 14,
               ),
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Intrinsic and Extrinsic semiconductors, Dependence of Fermi level on carrier-concentration and temperature, Carrier generation and recombination, Carrier transport: diffusion and drift, Hall effect, pn junction diode, Zener diode and their V-I Characteristics, Bipolar Junction Transistor (BJT): Construction, Principle of operation.",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
               ),
               Text(
-                'UNIT-III:  ',
+                'UNIT-III: Optoelectronics',
                 style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontSize: MediaQuery.of(context).size.width / 36,
                   fontWeight: FontWeight.bold,
+                  color: Colors.orange,
                 ),
               ),
               SizedBox(
-                height: 16,
+                height: 14,
               ),
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Radiative and non-radiative recombination mechanisms in semiconductors, LED and semiconductor lasers: Device structure, Materials, Characteristics and figures of merit, Semiconductor photodetectors: Solar cell, PIN and Avalanche and their structure, Materials, working principle and Characteristics ",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
               ),
               Text(
-                'UNIT-IV: ',
+                'UNIT-IV: Lasers and Fibre Optics',
                 style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontSize: MediaQuery.of(context).size.width / 36,
                   fontWeight: FontWeight.bold,
+                  color: Colors.orange,
                 ),
               ),
               SizedBox(
-                height: 16,
+                height: 14,
               ),
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Lasers: Introduction to interaction of radiation with matter, Coherence, Principle and working of Laser, Population inversion, Pumping, Types of Lasers: Ruby laser, Carbon dioxide (CO2) laser, He-Ne laser, Applications of laser. Fibre Optics: Introduction, Optical fibre as a dielectric wave guide, Total internal reflection, Acceptance angle, Acceptance cone and Numerical aperture, Step and Graded index fibres, Losses associated with optical fibres, Applications of optical fibres.",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
               ),
               Text(
-                'Unit-V: ',
+                'Unit-V: Electromagnetism and Magnetic Properties of Materials',
                 style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width / 30,
+                  fontSize: MediaQuery.of(context).size.width / 36,
                   fontWeight: FontWeight.bold,
+                  color: Colors.orange,
                 ),
               ),
               SizedBox(
-                height: 16,
+                height: 14,
               ),
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Laws of electrostatics, Electric current and the continuity equation, Ampere’s and Faraday’s laws, Maxwell’s equations, Polarisation, Permittivity and Dielectric constant, Internal fields in a solid, Clausius-Mossotti equation, Ferroelectrics and Piezoelectrics. Magnetisation, permeability and susceptibility, Classification of magnetic materials, Ferromagnetism and ferromagnetic domains, Hysteresis, Applications of magnetic materials. ",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
               ),
--- lib/src/department/ECE/year/year1/year1sem1/Subjects/EG.dart
+++ lib/src/department/ECE/year/year1/year1sem1/Subjects/EG.dart
@@ -10,15 +10,120 @@ class _EG extends State<EG> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: null,
-      body: Center(child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-        ],
-      )),
-
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'UNIT-I: Introduction to Engineering Drawing',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Principles of Engineering Graphics and their Significance, Conic Sections including the Rectangular Hyperbola – General method only. Cycloid, Epicycloid and Hypocycloid, Scales – Plain & Diagonal",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-II: Orthographic Projections',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Principles of Orthographic Projections – Conventions – Projections of Points and Lines, Projections of Plane regular geometric figures. Auxiliary Planes.",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-III: Projections of Regular Solids',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Projections of Regular Solids – Auxiliary Views - Sections or Sectional views of Right Regular Solids – Prism, Cylinder, Pyramid, Cone – Auxiliary views – Sections of Sphere ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'UNIT-IV: Development of Surfaces of Right Regular Solids',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Development of Surfaces of Right Regular Solids – Prism, Cylinder, Pyramid and Cone, Intersection of Solids: Intersection of – Prism vs Prism- Cylinder Vs Cylinder ",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Text(
+                'Unit-V: Isometric Projections',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Principles of Isometric Projection – Isometric Scale – Isometric Views – Conventions – Isometric Views of Lines, Plane Figures, Simple and Compound Solids – Isometric Projection of objects having non- isometric lines. Isometric Projection of Spherical Parts. Conversion of Isometric Views to Orthographic Views and Vice-versa –Conventions Introduction to CAD: (For Internal Evaluation Weightage only): Introduction to CAD Software Package Commands. - Free Hand Sketches of 2D- Creation of 2D Sketches by CAD Package",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
