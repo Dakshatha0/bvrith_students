06e4938abaa604989581b5d09776897e74ebdbab
UppalapatiSri <20wh1a0574@bvrithyderabad.edu.in>
2023-03-23 20:10:16 +0530

    ECE TOTAL COMPLETED WITHOUT ELECTIVES

--- lib/src/department/ECE/year/year4/year4sem1/Subjects/MOC.dart
+++ lib/src/department/ECE/year/year4/year4sem1/Subjects/MOC.dart
@@ -28,7 +28,7 @@ class _MOC extends State<MOC> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "  ",
+                  " Microwave Tubes: Limitations and Losses of conventional Tubes at Microwave Frequencies, Microwave Tubes – O Type and M Type Classifications, O-type Tubes: 2 Cavity Klystrons – Structure,  Reentrant Cavities, Velocity Modulation Process and Applegate Diagram, Bunching Process and Small Signal Theory – Expressions for O/P Power and Efficiency. Reflex Klystrons – Structure, Velocity Modulation and Applegate Diagram, Mathematical Theory of Bunching, Power Output, Efficiency,  Oscillating Modes and O/P Characteristics. Helix TWTs: Types and Characteristics of Slow Wave Structures; Structure of TWT and Amplification Process (qualitative treatment), Suppression of Oscillations, Gain Considerations. ",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -47,7 +47,7 @@ class _MOC extends State<MOC> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "M-Type Tubes: Introduction, Cross-field Effects, Magnetrons – Different Types, Cylindrical Traveling Wave Magnetron – Hull Cut-off and Hartree Conditions, Modes of Resonance and PI-Mode Operation, Separation of PIMode, o/p characteristics,  Microwave Solid State Devices: Introduction, Classification, Applications. TEDs – Introduction, Gunn Diodes – Principle, RWH Theory, Characteristics, Modes of Operation - Gunn Oscillation Modes,  Principle of operation of IMPATT and TRAPATT Devices.",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -66,7 +66,7 @@ class _MOC extends State<MOC> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Waveguide Components: Coupling Mechanisms – Probe, Loop, Aperture types. Waveguide Discontinuities – Waveguide Windows, Tuning Screws and Posts, Matched Loads. Waveguide Attenuators – Different Types, Resistive Card and Rotary Vane Attenuators; Waveguide Phase Shifters – Types, Dielectric and Rotary Vane Phase Shifters, Waveguide Multiport Junctions - E plane and H plane Tees. Ferrites– Composition and Characteristics, Faraday Rotation, Ferrite Components – Gyrator, Isolator,",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -85,7 +85,7 @@ class _MOC extends State<MOC> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Scattering matrix: Scattering Matrix Properties, Directional Couplers – 2 Hole, Bethe Hole, [s] matrix of Magic Tee and Circulator. Microwave Measurements: Description of Microwave Bench – Different Blocks and their Features,  Errors and Precautions, Measurement of Attenuation, Frequency. Standing Wave Measurements,  measurement of Low and High VSWR, Cavity Q, Impedance Measurements.",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -104,7 +104,7 @@ class _MOC extends State<MOC> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Optical Fiber Transmission Media: Optical Fiber types, Light Propagation, Optical fiber Configurations, Optical fiber classifications, Losses in Optical Fiber cables, Light Sources, Optical Sources, Light Detectors, LASERS, WDM Concepts, Optical Fiber System link budget.",
                   style: TextStyle(
                     fontSize: 16,
                   ),
--- lib/src/department/ECE/year/year4/year4sem1/Subjects/PPLE.dart
+++ lib/src/department/ECE/year/year4/year4sem1/Subjects/PPLE.dart
@@ -28,7 +28,7 @@ class _PPLE extends State<PPLE> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "  ",
+                  "Professional Practice and Ethics: Definition of Ethics, Professional Ethics - Engineering Ethics, Personal Ethics; Code of Ethics - Profession, Professionalism, Professional Responsibility, Conflict of Interest, Gift Vs Bribery, Environmental breaches, Negligence, Deficiencies in state-of-the-art; Vigil Mechanism, Whistle blowing, protected disclosures. Introduction to GST- Various Roles of Various Stake holders  ",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -47,7 +47,7 @@ class _PPLE extends State<PPLE> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Law of Contract: Nature of Contract and Essential elements of valid contract, Offer and Acceptance, Consideration, Capacity to contract and Free Consent, Legality of Object. Unlawful and illegal agreements, Contingent Contracts, Performance and discharge of Contracts, Remedies for breach of contract. Contracts-II: Indemnity and guarantee, Contract of Agency, Sale of goods Act -1930: General Principles, Conditions & Warranties, Performance of Contract of Sale.",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -66,7 +66,7 @@ class _PPLE extends State<PPLE> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Arbitration, Conciliation and ADR (Alternative Dispute Resolution) system: Arbitration – meaning, scope and types – distinction between laws of 1940 and 1996; UNCITRAL model law – Arbitration and expert determination; Extent of judicial intervention; International commercial arbitration; Arbitration agreements – essential and kinds, validity, reference and interim measures by court; Arbitration tribunal – appointment, challenge, jurisdiction of arbitral tribunal, powers, grounds of challenge, procedure and court assistance; Distinction between conciliation, negotiation, mediation and arbitration, confidentiality,  resort to judicial proceedings, costs; Dispute Resolution Boards; Lok Adalats.",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -85,7 +85,7 @@ class _PPLE extends State<PPLE> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Engagement of Labour and Labour & other construction-related Laws: Role of Labour in Civil Engineering; Methods of engaging labour- on rolls, labour sub-contract, piece rate work; Industrial Disputes Act, 1947; Collective bargaining; Industrial Employment (Standing Orders) Act, 1946; Workmen’s Compensation Act, 1923; Building & Other - Construction Workers (regulation of employment and conditions of service) Act (1996) and Rules (1998); RERA Act 2017, NBC 2017.",
                   style: TextStyle(
                     fontSize: 16,
                   ),
@@ -104,7 +104,7 @@ class _PPLE extends State<PPLE> {
               Container(
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: Text(
-                  "",
+                  "Law relating to Intellectual property: Introduction – meaning of intellectual property, main forms of IP, Copyright, Trademarks, Patents and Designs, Secrets; Law relating to Copyright in India including Historical evolution of Copy Rights Act, 1957, Meaning of copyright – computer programs, Ownership of copyrights and assignment, Criteria of infringement, Piracy in Internet – Remedies and procedures in India; Law relating to Patents under Patents Act, 1970",
                   style: TextStyle(
                     fontSize: 16,
                   ),
